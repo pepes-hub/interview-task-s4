f460a65b4b22ba4ea273a0488016aff6c5285052
Paweł Podsiadły <pawel.podsiadly@protonmail.com>
2023-11-21 17:38:31 +0100

    feat: introduce NavBar

---
+++ src/app/shared/ui/NavBar/NavBar.tsx
@@ -0,0 +1,10 @@
+import { AppBar } from "@mui/material";
+import { LogoSvg } from "../../../assets/images";
+
+export const NavBar = () => {
+  return (
+    <AppBar position={"relative"}>
+      <LogoSvg />
+    </AppBar>
+  );
+};
