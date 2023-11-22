00d17d271087e35dc5106a8ce84b9054a852e145
Paweł Podsiadły <pawel.podsiadly@protonmail.com>
2023-11-22 15:56:05 +0100

    feat: introduce translations

---
+++ src/app/assets/translations/en.ts
@@ -0,0 +1,15 @@
+const en = {
+  translation: {
+    labels: {
+      theGivingBlock: "The Giving Block",
+    },
+    donationsModal: {
+      title: "The giving block",
+      subtitle: "Set up your donation goal!",
+    },
+    actions: {},
+    placeholders: {},
+  },
+};
+
+export default en;
