ed20201ffc0284f843073aa7476d68117d23e27d
Paweł Podsiadły <pawel.podsiadly@protonmail.com>
2023-11-23 14:36:35 +0100

    feat: DonationsModalContent summary block

--- src/app/features/DonationsModal/components/DonationsModalContent/DonationsModalContent.styles.ts
+++ src/app/features/DonationsModal/components/DonationsModalContent/DonationsModalContent.styles.ts
@@ -14,4 +14,56 @@ export const donationsModalContentStyles = (theme: Theme) => ({
   button: {
     width: "100%",
   },
+  summaryBlock: {
+    marginBottom: "32px",
+    [theme.breakpoints.down("sm")]: {
+      border: "1px solid",
+      borderColor: "lightGrey.main",
+    },
+  },
+  summaryRow: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    padding: "0 16px",
+    color: "blueGrey.main",
+    alignItems: "center",
+    marginBottom: "24px",
+    [theme.breakpoints.down("sm")]: {
+      padding: "24px 16px 0px 16px",
+    },
+  },
+  summaryTitle: {
+    fontWeight: "500",
+    fontSize: "20px",
+    lineHeight: "24px",
+    [theme.breakpoints.down("sm")]: {
+      fontSize: "16px",
+      lineHeight: "19.2px",
+    },
+  },
+  totalAmount: {
+    fontSize: "32px",
+    lineHeight: "38.4px",
+    fontWeight: "700",
+    [theme.breakpoints.down("sm")]: {
+      fontSize: "24px",
+      lineHeight: "28.8px",
+    },
+  },
+  summary: {
+    padding: "24px 16px",
+    backgroundColor: "lightGrey.main",
+    borderRadius: "5px",
+  },
+  summaryLabel: {
+    lineHeight: "16.8px",
+    fontSize: "12px",
+    strong: {
+      fontFamily: "Inter",
+      fontWeight: "700",
+    },
+    [theme.breakpoints.down("sm")]: {
+      textAlign: "center",
+    },
+  },
 });
--- src/app/features/DonationsModal/components/DonationsModalContent/DonationsModalContent.tsx
+++ src/app/features/DonationsModal/components/DonationsModalContent/DonationsModalContent.tsx
@@ -1,6 +1,9 @@
-import { Box, Button, Stack, useTheme } from "@mui/material";
+import { Box, Button, Stack, Typography, useTheme } from "@mui/material";
 import { donationsModalContentStyles } from "./DonationsModalContent.styles";
-import { useTranslation } from "react-i18next";
+import { Trans, useTranslation } from "react-i18next";
+import moment from "moment";
+import { SHORT_DATE_FORMAT } from "../../../../constants/date";
+import { amountFormat } from "../../../../shared/helpers/amountFormat";
 
 export const DonationsModalContent = () => {
   const theme = useTheme();
@@ -8,8 +11,34 @@ export const DonationsModalContent = () => {
 
   const { t } = useTranslation();
 
+  const totalAmount = 200000;
+  const date = moment().format(SHORT_DATE_FORMAT);
+
+  const formattedTotalAmount = amountFormat(totalAmount);
+
   return (
     <Box sx={styles.container}>
+      <Stack sx={styles.summaryBlock}>
+        <Stack sx={styles.summaryRow}>
+          <Typography sx={styles.summaryTitle}>
+            {t("donationsModal.summaryTitle")}
+          </Typography>
+          <Typography sx={styles.totalAmount}>
+            {formattedTotalAmount}
+          </Typography>
+        </Stack>
+        <Box sx={styles.summary}>
+          <Typography sx={styles.summaryLabel}>
+            <Trans
+              i18nKey="donationsModal.summary"
+              values={{
+                amount: formattedTotalAmount,
+                date,
+              }}
+            />
+          </Typography>
+        </Box>
+      </Stack>
       <Stack sx={styles.buttonsContainer}>
         <Button variant={"contained"} sx={styles.button} color={"secondary"}>
           {t("actions.cancel")}
