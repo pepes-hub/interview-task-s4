45d7b0d7a5ed5486a7249bc7f9d21dbed72bd897
Paweł Podsiadły <pawel.podsiadly@protonmail.com>
2023-11-24 13:53:31 +0100

    feat: basic en translations

--- src/app/assets/translations/en.ts
+++ src/app/assets/translations/en.ts
@@ -2,13 +2,20 @@ const en = {
   translation: {
     labels: {
       theGivingBlock: "The Giving Block",
+      iCanDonate: "I can donate",
+      donateUntil: "Every month until",
     },
     donationsModal: {
       title: "The giving block",
       subtitle: "Set up your donation goal!",
+      summaryTitle: "Total amount",
+      summary:
+        "You will be sending <strong>{{ amount }}</strong> every month, until <strong>{{ date }}</strong>. Thank you!",
     },
-    actions: {},
-    placeholders: {},
+    actions: {
+      cancel: "Cancel",
+      continue: "Continue",
+    },
   },
 };
 
